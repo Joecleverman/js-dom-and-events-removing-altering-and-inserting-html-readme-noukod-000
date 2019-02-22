aedc56d4c8e06b3bf9e634d9141afdb426967d79
Josue Abraham <josuea@noukod.com>
2019-02-22 17:06:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var element = document.createElement('div')
+let element = document.createElement('div')
 element.innerHTML = 'Hello, DOM!'
 element.style.backgroundColor = '#f9f9f9';
 document.body.appendChild(element)
 element.style.textAlign = 'center';
-var ul = document.createElement('ul')
+let ul = document.createElement('ul')
 
 for (let i = 0; i < 3; i++) {
   let li = document.createElement('li')
@@ -13,4 +13,4 @@ for (let i = 0; i < 3; i++) {
 
 element.appendChild(ul)
 ul.style.textAlign = 'left'
-l.removeChild(ul.querySelector('li:nth-child(2)'))
+ul.removeChild(ul.querySelector('li:nth-child(2)'))
